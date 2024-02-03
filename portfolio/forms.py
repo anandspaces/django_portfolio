931f6aa43e3552dd276905dd4a15514b72fc96cb
anandspaces <anandvaibhaw000@gmail.com>
2024-02-04 04:25:11 +0530

    adding styles in forms

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,5 +1,5 @@
 # new
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Audience
 
 
@@ -7,4 +7,8 @@ class AudienceForm(ModelForm):
     class Meta:
         model = Audience
         fields = '__all__'
-        
+        widgets = {
+            'name':TextInput(attrs={'class':'form-control'}),
+            'email':TextInput(attrs={'class':'form-control'}),
+            'message':TextInput(attrs={'class':'form-control'}),
+        }
