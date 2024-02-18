5b652765085e87b9fa947019ea80156125987e99
anandspaces <anandvaibhaw000@gmail.com>
2024-02-18 19:22:25 +0530

    added resume functioning button

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,8 @@
 # new
 from django.urls import path
-from .views import index
+from .views import index,download_file
 
 urlpatterns = [
     path('',index,name='home'),
+    path('download-resume/', download_file, name='download_resume'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-# from django.views.generic import TemplateView # new
 from .forms import AudienceForm # new
-
-# Create your views here.
-# class HomePageView(TemplateView):
-#     template_name = 'index.html'
+from django.http import FileResponse,HttpResponse,HttpResponseNotFound #new
+from django.conf import settings # new
+import os
+import mimetypes
 
 def index(request):
     form = AudienceForm()
@@ -14,3 +13,24 @@ def index(request):
             form.save()
     context = {'form':form}
     return render(request,'index.html',context)
+
+def download_resume(request):
+    # Path to the resume PDF file
+    resume_path = os.path.join(settings.STATIC_ROOT, 'files', 'resume.pdf')
+    
+    # Open the file and return it as a FileResponse
+    return FileResponse(open(resume_path, 'rb'), as_attachment=True, filename='resume.pdf')
+
+def download_file(request):
+    # Fill these variables with real values
+    fl_path = 'static/files/resume.pdf'  # Adjust the path to match the actual location of your resume PDF file
+    filename = 'resume.pdf'
+
+    if os.path.exists(fl_path):
+        with open(fl_path, 'rb') as fl:
+            mime_type, _ = mimetypes.guess_type(fl_path)
+            response = HttpResponse(fl, content_type=mime_type)
+            response['Content-Disposition'] = "attachment; filename=%s" % filename
+            return response
+    else:
+        return HttpResponseNotFound("File not found")
